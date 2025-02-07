3c081a46565aee40d26631e90b954479da672c88
louilat <lmm.latournerie@gmail.com>
2025-02-07 13:50:30 +0100

    add balances in usd

--- src/balances_collector/balances_collector.py
+++ src/balances_collector/balances_collector.py
@@ -63,12 +63,20 @@ class AaveV3RawBalancesCollector:
             "liquidityRate",
             "variableBorrowRate",
             "lastUpdateTimestamp",
+            "underlyingTokenPriceUSD",
         ]
-        response, _ = self.data_provider_contract.functions.getReservesData(
-            "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e"
-        ).call()
-        response = [reserve_data[0:17] for reserve_data in response]
+        response, base_currency_info = (
+            self.data_provider_contract.functions.getReservesData(
+                "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e"
+            ).call()
+        )
+        response = [
+            reserve_data[0:17] + tuple((reserve_data[22],)) for reserve_data in response
+        ]
         reserves_data = DataFrame(response, columns=reserve_data_columns)
+        reserves_data.underlyingTokenPriceUSD = (
+            reserves_data.underlyingTokenPriceUSD / 10 ** base_currency_info[-1]
+        )
 
         self.reserves_data = reserves_data
         return reserves_data
@@ -85,6 +93,7 @@ class AaveV3RawBalancesCollector:
             "usageAsCollateralEnabled",
             "liquidityIndex",
             "variableBorrowIndex",
+            "underlyingTokenPriceUSD",
         ]
         processed_balances = self.all_users_balances.merge(
             self.reserves_data[merge_columns], how="left", on="underlyingAsset"
@@ -95,13 +104,22 @@ class AaveV3RawBalancesCollector:
 
         processed_balances["currentATokenBalance"] = (
             processed_balances.scaledATokenBalance
-            / processed_balances.decimals
+            / 10**processed_balances.decimals
             * processed_balances.liquidityIndex
         )
         processed_balances["currentVariableDebt"] = (
             processed_balances.scaledVariableDebt
-            / processed_balances.decimals
+            / 10**processed_balances.decimals
             * processed_balances.variableBorrowIndex
+        )
+
+        processed_balances["currentATokenBalanceUSD"] = (
+            processed_balances.currentATokenBalance
+            * processed_balances.underlyingTokenPriceUSD
+        )
+        processed_balances["currentVariableDebtUSD"] = (
+            processed_balances.currentVariableDebt
+            * processed_balances.underlyingTokenPriceUSD
         )
 
         self.processed_balances = processed_balances
